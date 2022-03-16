60dd3ac34d786bb88fd163c293e0b9693faae4e8
Mr. Roach <julianroach.dev@gmail.com>
2022-03-16 14:16:08 +0100

    Add project files.

---
+++ TODO-list/source/main.cpp
@@ -0,0 +1,26 @@
+/**************************************************************************************************
+TODO-list - A TODO list program. Made with DOSELTA.
+
+The Description:
+
+Reason: 
+
+This project isn't meant to be used by anyone, but if you like it, and you would like to "upgrade"
+it a little, feel free to pull request your changes!
+**************************************************************************************************/
+
+#include <iostream>
+#include "macros.h"
+
+#include <DOSELTA.h>
+
+int main()
+{
+	DOS::Prepare();
+
+
+
+	LOG("Success; hello world!");
+
+	return 0;
+}
